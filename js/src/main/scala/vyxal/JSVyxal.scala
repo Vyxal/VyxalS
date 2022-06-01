c65f2c02023519efe206503abe995506b4b0a0c0
cgccuser <0foobarbaz@gmail.com>
2022-06-01 22:01:55 +0000

    Copy over js, html, and css files

--- js/src/main/scala/vyxal/JSVyxal.scala
+++ js/src/main/scala/vyxal/JSVyxal.scala
@@ -9,10 +9,14 @@ object JSVyxal extends js.Object {
       code: String,
       inputs: js.Array[VAny],
       flags: js.Array[String],
-      outputFn: js.Function1[String, Unit]
+      outputFn: js.Function1[String, Unit],
+      warnFn: js.Function1[String, Unit],
+      errFn: js.Function1[String, Unit]
   ): Unit = {
     given Backend with {
       override def print(s: String) = outputFn(s)
+      override def warn(s: String) = warnFn(s)
+      override def err(s: String) = errFn(s)
     }
     Interpreter.execute(code, inputs.toList, flags.toList)
   }
